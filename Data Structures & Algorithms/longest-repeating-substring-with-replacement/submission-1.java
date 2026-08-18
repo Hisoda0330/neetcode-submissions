b265cef0c8389ca06a238f130acbb363920a39b2
Jinpeng Huang <123497012+Hisoda0330@users.noreply.github.com>
2026-08-18 14:55:43 -0700

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int left = 0;
+        int maxRes = 0;
+        HashMap<Character, Integer> count = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            count.put(c,count.getOrDefault(c,0) + 1);
+            int mostFrequent = Collections.max(count.values());
+            if((i - left + 1) - mostFrequent > k){
+                char update = s.charAt(left);
+                count.put(update, count.get(update)-1);
+                left ++;
+            }
+            maxRes = Math.max(maxRes, i - left + 1);
+        }
+        return maxRes;
+    }
+//core idea:: for each input, record the read value into a hashmap. check if window shrink is needed by comparing if subtracting the length by the most frequent char in the window > replacement k, then shrink window by ++ left ponter.
+}
