97d27a3acd4ba3685f154f3abecbb4c4b74850f2
Jinpeng Huang <123497012+Hisoda0330@users.noreply.github.com>
2026-08-19 17:53:48 -0700

    Add: minimum-window-with-characters - submission-4

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-4.java
@@ -0,0 +1,51 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        HashMap<Character, Integer> needMap = new HashMap<>();
+        for(int i = 0; i < t.length(); i++){
+            char a = t.charAt(i);
+            needMap.put(a, needMap.getOrDefault(a,0) + 1);
+        }
+        
+        int left = 0 , resleft = 0;
+        int need = needMap.size();
+        int have = 0;
+        int reslen = Integer.MAX_VALUE;
+        HashMap<Character, Integer> window = new HashMap<>();
+        for(int right = 0; right < s.length(); right++){
+
+            char b = s.charAt(right);
+
+            window.put(b,window.getOrDefault(b,0)+1);
+
+            if(window.get(b).equals(needMap.get(b))){
+                have++;
+            }
+
+            while(have == need){
+
+                int foundlen = right - left + 1;
+
+                if(foundlen < reslen){
+                    resleft = left;
+                    reslen = foundlen;
+                }
+
+                window.put(s.charAt(left), window.getOrDefault(s.charAt(left),0) - 1);
+
+                if(needMap.containsKey(s.charAt(left)) && window.get(s.charAt(left)) <
+                                    needMap.get(s.charAt(left))){
+                    have--;
+                }
+
+                left ++;
+            }
+        }
+        if(reslen == Integer.MAX_VALUE){
+            return "";
+        }
+        else{
+            return s.substring(resleft, resleft + reslen);
+        }
+
+    }
+}
