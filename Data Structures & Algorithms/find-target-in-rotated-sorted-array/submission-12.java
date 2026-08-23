7343bac149d73521c26691e78a0086785e2815e2
Jinpeng Huang <123497012+Hisoda0330@users.noreply.github.com>
2026-08-23 16:28:41 -0700

    Add: find-target-in-rotated-sorted-array - submission-12

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-12.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while(low <= high){
+            int mid = low + ((high - low) / 2);
+            
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            //left side is sorted
+            else if(nums[low] <= nums[mid]){
+                if(target >= nums[low] && target < nums[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+
+            //right side is sorted
+            else{
+                if(target <= nums[high] && target > nums[mid]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+
+        }
+
+        return -1;
+    }
+}
+
+/*
+core idea: know which side is sorted, and shrink the window accordingly
+1. make sure to understand the bound as the window shrinks
+
+*/
