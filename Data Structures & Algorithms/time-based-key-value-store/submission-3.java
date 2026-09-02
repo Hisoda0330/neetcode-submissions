062a941c4a520489136c796b4ee47b6655e0b2c8
Jinpeng Huang <123497012+Hisoda0330@users.noreply.github.com>
2026-09-02 16:29:22 -0700

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.java
@@ -0,0 +1,57 @@
+class TimeMap {
+
+
+    private Map<String, List<Pair>> store;
+
+    //class for Pair so can use later on in the list 
+    private static class Pair{
+        String value;
+        int timestamp;
+        
+        Pair(String value, int timestamp){
+            this.value = value;
+            this.timestamp = timestamp;
+        }
+    }
+
+    public TimeMap() {
+
+        store = new HashMap<>();
+
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if(!store.containsKey(key)){
+            store.put(key, new ArrayList<>());
+        }
+        store.get(key).add(new Pair(value, timestamp));
+    }
+    
+    public String get(String key, int timestamp) {
+        
+        List<Pair> value = store.getOrDefault(key, Collections.emptyList());
+
+        String result = "";
+        int left = 0;
+        int right = value.size() - 1;
+        
+        //use standard binary search
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            
+            //ans on the right bc timestamp larger than mid
+            if(value.get(mid).timestamp <= timestamp){
+                result = value.get(mid).value;
+                left = mid + 1;
+            }//else would be on the left bc timestamp smaller than mid
+            else{
+                right = mid - 1;
+            }
+         }
+
+         return result;
+         
+    }
+}
+//An interface defines a type and its required behavior; a class implements that interface and creates objects of a concrete type.
+//core idea: understand the class, list(interface), hashmap(class), map(interface), and arraylist(class) in java and use standard binary search.
