ccb7f6c8ebb1c635081d83f5be9827be69d8741c
Jinpeng Huang <123497012+Hisoda0330@users.noreply.github.com>
2026-09-21 15:49:25 -0700

    Add: linked-list-cycle-detection - submission-1

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-1.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+/*
+Use slow/fast pointer (Floyd's cycle detection) technique.
+check if fast will ever be null, if not null -> cycle, else no cycle
+care for nullpointer exception for fast.next.next, null.next = nullpointerexception
+*/
